4de2bc61f3edf0e852a2af3cd19258bbba998f3b
daniel <tomas.madan21@zaragoza.salesianos.edu>
2023-06-18 15:54:49 +0200

    Eliminando proyecto sin uso

--- WorkFlowXAPI/WorkFlowXAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WorkFlowXAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- WorkFlowXAPI/WorkFlowXAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WorkFlowXAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
